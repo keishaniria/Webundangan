658e53383ba3a7c4830f5befc86494719451bbdd
unknown <keishaniria@gmail.com>
2024-12-20 07:52:28 +0700

    first commit

---
+++ assets/js/web.js
@@ -0,0 +1,27 @@
+function bukaUndangan(){
+    const s1 = document.querySelector('#container');
+    const s2 = document.querySelector('#quote');
+    const s3 = document.querySelector('#profile');
+    const s4 = document.querySelector('#navbar');
+    const s5 = document.querySelector('#tempat-waktu');
+    const s6 = document.querySelector('#section4');
+    const s7 = document.querySelector('#section5');
+    const s8 = document.querySelector('#rsvp');
+    const s9 = document.querySelector('#story');
+    const s10 = document.querySelector('#gift');
+    const s11 = document.querySelector('#wishes');
+    const s12 = document.querySelector('#thankyou');
+s1.style.display = 'none'
+s2.style.display = 'block'
+s3.style.display = 'block'
+s4.style.display = 'block'
+s5.style.display = 'block'
+s6.style.display = 'block'
+s7.style.display = 'block'
+s8.style.display = 'block'
+s9.style.display = 'block'
+s10.style.display = 'block'
+s11.style.display = 'block'
+s12.style.display = 'block'
+}
+
